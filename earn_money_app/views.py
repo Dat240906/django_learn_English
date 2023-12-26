64131891b417a493458ff62c9d361a45670dfab2
proj7436 <umbalaavc16@gmail.com>
2023-12-26 22:45:09 +0700

    test API in host

--- earn_money_app/views.py
+++ earn_money_app/views.py
@@ -5,7 +5,7 @@ from django.views import View
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
 from home_app.models import UserModel
-from .models import GiftCodeModel, SiteRewardTempModel, SiteRewardTempModel
+from .models import GiftCodeModel,linkWeb1sStorage, SiteRewardTempModel, SiteRewardTempModel
 from home_app.views import get_user_ip
 from django.core.cache import cache
 import time
@@ -14,23 +14,48 @@ import time
 
 
 class EarnMoneySite(View):
-    
+    def getInfoUser(self, request):
+        """get username, money, noties, avatar"""
+        access_token = request.session['userModel']['access_token']
+
+        cache_key_user = f'test{access_token}'
+        
+        cache_data_user = cache.get(cache_key_user)
+
+        cache_key_noti = 'noti'
+        cache_data_noti = cache.get(cache_key_noti)
+
+        
+        username = cache_data_user['user'].username
+        
+        money = cache_data_user['user'].money
+        noties = cache_data_noti['noties']
+        avatar =cache_data_user['user'].avatar
+
+        return username, money, noties, avatar
     def get(self, request):
-        ip_user = get_user_ip(request)
-        cache_key_user = f'test{ip_user}'
-          #check cache, chưa có sẽ về login
         try:
-            context = {
-                'coin_in_day':self.coin_day(request),
-              
-                'coin_total':cache.get(cache_key_user)['user'].money
-                
-            }
+            # ip_user = get_user_ip(request)
+            access_token = request.session['userModel']['access_token']
+            cache_key_user = f'test{access_token}'
+
+            username, money, noties, avatar = self.getInfoUser(request)
+            #check cache, chưa có sẽ về login
+            try:
+                context = {
+                    'coin_in_day':self.coin_day(request),
+                    'username':username,
+                    'money':money,
+                    'noties':noties,
+                    'avatar':avatar,
+                    'coin_total':cache.get(cache_key_user)['user'].money
+                    
+                }
+            except TypeError:
+                    return redirect('login')
+            return render(request, 'earn_money_app.html', context = context)
         except TypeError:
-                return redirect('login')
-        return render(request, 'earn_money_app.html', context = context)
-
-
+            return redirect('login')
     def time_reset_in_day(self, hour, minute, second):
         default_hour = 24
         default_min = 60
@@ -78,32 +103,77 @@ class EarnMoneySite(View):
 
 
 
-class EarnMoneyWeb1s(View):
+class EarnMoneyWeb1s(EarnMoneySite, View):
     def get(self, request):
-        return render(request, 'earn_money_web1s.html')
+        try:
+            username, money, noties, avatar = self.getInfoUser(request)
+        except TypeError:
+            return redirect('login')
+
+
+        context = {
+            'username':username,
+            'money':money,
+            'noties':noties,
+            'avatar':avatar
+        }
+        return render(request, 'earn_money_web1s.html', context = context)
 
 
 
 
 
 
-class EarnMoneyYoutube(View):
+class EarnMoneyYoutube(EarnMoneySite, View):
     def get(self, request):
-        return render(request, 'earn_money_youtube.html')
+        try:
+            username, money, noties, avatar = self.getInfoUser(request)
+        except TypeError:
+            return redirect('login')
+
+        context = {
+            'username':username,
+            'money':money,
+            'noties':noties,
+            'avatar':avatar
+        }
+        return render(request, 'earn_money_youtube.html', context = context)
 
 
 
 
 
-class EarnMoneyTiktok(View):
+class EarnMoneyTiktok(EarnMoneySite, View):
     def get(self, request):
-        return render(request, 'earn_money_tiktok.html')
+        try:
+            username, money, noties, avatar = self.getInfoUser(request)
+        except TypeError:
+            return redirect('login')
+
+        context = {
+            'username':username,
+            'money':money,
+            'noties':noties,
+            'avatar':avatar
+        }
+        return render(request, 'earn_money_tiktok.html', context = context)
     
 
 
-class EarnMoneyFacebook(View):
+class EarnMoneyFacebook(EarnMoneySite, View):
     def get(self, request):
-        return render(request, 'earn_money_facebook.html')
+        try:
+            username, money, noties, avatar = self.getInfoUser(request)
+        except TypeError:
+            return redirect('login')
+
+        context = {
+            'username':username,
+            'money':money,
+            'noties':noties,
+            'avatar':avatar
+        }
+        return render(request, 'earn_money_facebook.html', context = context)
 
 
 
@@ -129,7 +199,9 @@ class EarnMoneyGetCoin(APIView):
        
         try:
             #save cache
-            cache_key_user = f'test{user.ip_address}'
+            access_token = request.session['userModel']['access_token']
+
+            cache_key_user = f'test{access_token}'
             cache_data_user = cache.get(cache_key_user)
             cache_data_user['user'].money += gift_in_DB.value
             cache.set(cache_key_user, cache_data_user)
@@ -199,7 +271,7 @@ class HandlePlusMoneyByAccessToken(APIView):
 
         if request_from_client == 'collect':
             
-            access_token = request.POST['access_token']
+            access_token = request.session['userModel']['access_token']
 
             try:
                 user = UserModel.objects.get(access_token = access_token)
@@ -212,7 +284,9 @@ class HandlePlusMoneyByAccessToken(APIView):
         
             try:
                 #save cache
-                cache_key_user = f'test{user.ip_address}'
+                access_token = request.session['userModel']['access_token']
+
+                cache_key_user = f'test{access_token}'
                 cache_data_user = cache.get(cache_key_user)
                 cache_data_user['user'].money += 0.1
                 cache.set(cache_key_user, cache_data_user)
@@ -246,4 +320,28 @@ class CreateGiftcode(APIView):
         return JsonResponse({
             'status':'success',
             'message':'Tạo thành công, hãy sao chép giftcode và nhập ngay!'
+        })
+    
+
+
+
+
+class getJobWeb1sAPI(APIView):
+    def get(self, request):
+        access_token = request.session['userModel']['access_token']
+
+        all_job = linkWeb1sStorage.objects.all()
+        list_job_of_user = []
+        for job in all_job:
+            access_token_in_job = job.list_access_token_user_retrieved.get(access_token, False)
+
+            #tìm job mà user chưa làm 
+            if not access_token_in_job:
+
+                list_job_of_user.append(job)
+
+        jobs = [job.link for job in list_job_of_user]
+        
+        return JsonResponse({
+            'jobs':jobs
         })
